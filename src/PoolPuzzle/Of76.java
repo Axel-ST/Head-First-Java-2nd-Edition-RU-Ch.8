8b29bd45a6a7ee32d9b8c7611641057e91793977
Axel-ST <Kozlov.axe@gmail.com>
2023-03-17 20:56:46 +0700

    Create all needed class files.

---
+++ src/PoolPuzzle/Of76.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Axel_ST on 10.03.2023
+ *
+ *  Head First Java, 2nd Edition (RU) p. 262
+ */
+
+package PoolPuzzle;
+
+public class Of76 extends Clowns{
+    public static void main(String[] args) {
+    
+    }
+}
